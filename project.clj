ca39dc2ab22a1758735ebb97e9ee2008719bca4a
Jeff Weiss <jweiss@redhat.com>
2010-11-11 20:23:13 -0500

    first commit - got most necessary deps in project.clj

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject flickr2picasa "1.0.0-SNAPSHOT"
+  :description "Flickr to Google photo transfer"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [clj-http "0.1.1"]
+                 [com.google.gdata/gdata-photos-2.0 "1.41.5"]]
+  :repositories {"mandubian-mvn" "http://mandubian-mvn.googlecode.com/svn/trunk/mandubian-mvn/repository"})
