5d49f964b20fa68398d1a25261a20b73f171ac84
Jeff Weiss <jweiss@redhat.com>
2010-11-14 18:41:52 -0500

    more flickr api work

--- src/flickr.clj
+++ src/flickr.clj
@@ -1,12 +1,11 @@
 (ns flickr
-  (:require [clj-http.client :as http])
+  (:require [clj-http.client :as http]
+	    [clojure.xml :as xml]
+	    [clojure.zip :as zip]
+	    [clojure.contrib.zip-filter.xml :as zf])
   (:import [java.security MessageDigest]
-	   [org.apache.commons.codec.binary Hex]))
-
-(defn flickr-api-call
-  [method params]
-  (http/get "http://api.flickr.com/services/rest"
-	    {:query-params (assoc params :method method) }))
+	   [org.apache.commons.codec.binary Hex]
+	   [java.io ByteArrayInputStream]))
 
 (def md5-digest (MessageDigest/getInstance "MD5"))
 
@@ -15,5 +14,34 @@
 	byte-hash (.digest md5-digest utf8bytes)]
     (String. (Hex/encodeHex byte-hash))))
 
-(comment (defn sorted-params [params]
-	  (sorted-map) ))
+(defn sorted-param-str [params]
+  (let [m (apply sorted-map (interleave (map name (keys params))
+			      (vals params)))]
+    (apply str (mapcat identity m))))
+
+(defn string-keys [m]
+  (zipmap (map name (keys m)) (vals m)))
+
+(defn api-sig [secret params]
+  (md5 (str secret (sorted-param-str params))))
+
+(defn flickr-api-call
+  ([method params secret]
+     (let [params+method (assoc params :method method)
+	   params+sig (if secret (assoc params+method :api_sig (api-sig secret params+method))
+			  params+method)]
+       (http/get "http://api.flickr.com/services/rest"
+			     {:query-params (string-keys params+sig) })))
+  ([method params]
+      (flickr-api-call method params nil)))
+
+(defn xml-tags [s tag]
+  (zf/xml-> (zip/xml-zip (xml/parse (ByteArrayInputStream. (.getBytes s)))) tag zf/text))
+
+(defn response-field [method params secret field]
+  (-> (flickr-api-call method params secret) :body (xml-tags field)))
+
+(defn token [api-key secret]
+  (let [key {:api_key api-key}
+	frob (first (response-field "flickr.auth.getFrob" key secret :frob))]
+    (first (response-field "flick.auth.getToken" (assoc key :frob frob) secret :token))))
